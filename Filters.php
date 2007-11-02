036b72770798f49831e64d0d78666aa979afee2f
postal2600 <postal2600@yahoo.com>
2007-11-02 19:23:07 +0000

    obfuscater: fixed a small validation issue.

--- Filters.php
+++ Filters.php
@@ -20,7 +20,7 @@ function filter_obfuscate($input)
 {
     $text = preg_split("//",$input);
     $text = implode("'+'",$text);
-    $result = "<script type='text/javascript' src='phpDrone/res/scripts/obfuscater.js' /></script>\n";
+    $result = "<script type='text/javascript' src='phpDrone/res/scripts/obfuscater.js'></script>\n";
     $result .= "<script type='text/javascript'>obfuscate('{$text}')</script>";
     return $result;
 }
